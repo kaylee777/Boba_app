2ff0831ad1359ad7a1b81364bbe8ca6707271087
Kaylee Miller <98123192+kaylee777@users.noreply.github.com>
2022-09-27 15:25:08 -0500

    Boba Review App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatTable } from '@angular/material/table';
+ 
 
 @Component({
-  selector: 'app-root',
+selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'boba-app';
+  title = 'Boba-App';
+  data: Array<any>;
+
+constructor() {
+    this.data = [
+        { RestaurantName: 'Tao Cha', Drink: 'Matcha Milk tea Latte', Review: '5/5' },
+        { RestaurantName: 'Teapioca', Drink: 'Brown Sugar Milk Tea', Review: '2/5' },
+        { RestaurantName: 'Feng Cha', Drink: 'Matcha Milk tea Latte', Review: '3/5' },
+       
+    ];
+}
+
+
+
 }
