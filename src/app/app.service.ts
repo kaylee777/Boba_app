a019658eaed54e1842a74b39e26a518e22905ac9
Kaylee Miller <98123192+kaylee777@users.noreply.github.com>
2022-10-05 14:10:20 -0500

    updated comments

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,21 +1,28 @@
 import { Inject, Injectable } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
 export class Boba {
   static splice: any;
   constructor(public id: string, public RestaurantName: string, public Drink: string, public Review: string) { }
 }
+//ensures that the compiler will generate the necessary metadata to create the class's dependencies when the class is injected.
 @Injectable({
   providedIn: 'root'
 })
+//allows AppService to be used in other modules in the app.
+//filler data for application to demo.
 export class AppService {
   data: Boba[] = [
     new Boba("1", "Tao Cha", "Matcha Milk tea Latte", "5/5"),
     new Boba("2", "Teapioca", "Brown Sugar Milk tea", "2/5"),
     new Boba("3", "Feng Cha", "Matcha Milk tea Latte", "3/5"),
   ]
-
+  // used to inject dependencies into the component class, creating a new instance.
   constructor() { }
-
+  //new instance "updatedData", mapping through each element in the array
+  //passing in a new instance of Boba called bobaItem
+  //if the id of info matches the id of bobaItem, it will return the data thats already been passed. 
+  //otherwise it will return new instance of data called bobaItem
   onRowEdit(info: Boba) {
     const updatedData = this.data.map((bobaItem) => {
       if (info.id === bobaItem.id) {
@@ -25,12 +32,9 @@ export class AppService {
     })
     this.data = updatedData
   }
-
   deleteDataRow(id: string) {
     this.data = this.data.filter((bobaItem) => bobaItem.id !== id);
-
   }
-
   create(boba: Boba) {
     this.data.push(boba)
   }
--- src/app/box-dialog/box-dialog.component.ts
+++ src/app/box-dialog/box-dialog.component.ts
@@ -5,23 +5,25 @@ import { AppComponent } from '../app.component';
 import { AppService, Boba } from '../app.service';
 import { Input } from '@angular/core';
 import { v4 as uuid } from 'uuid';
-
+//building blocks for the application 
 @Component({
   selector: 'app-box-dialog',
   templateUrl: './box-dialog.component.html',
   styleUrls: ['./box-dialog.component.css']
 })
+//allows BDC to be used in other modules in the application.
 export default class BoxDialogComponent implements OnInit {
   name: string = ""
   drink: string = ""
   review: string = ""
-
+  // used to inject dependencies into the component class, creating a new instance.
   constructor(
     private dialogRef: MatDialogRef<BoxDialogComponent>,
     private service: AppService,
     @Inject(MAT_DIALOG_DATA) public data?: Boba
   ) { }
-
+  // place to put the code that we need to be executed as soon as the class is instantiated.
+  //if this data is not null, we want to edit, the current row. 
   ngOnInit(): void {
     console.log(this.data)
     if (this.data != null) {
@@ -30,7 +32,7 @@ export default class BoxDialogComponent implements OnInit {
       this.review = this.data.Review
     }
   }
-
+  //called on HTML to respond to a click to close the dialog.
   closeDialog(): void {
     this.dialogRef.close()
   }
@@ -38,23 +40,23 @@ export default class BoxDialogComponent implements OnInit {
   handleSave() {
     this.closeDialog()
     const info: Boba = {
+      //universal unique identifier to generate an id.
+      // '?' used to say the id won't always be defined.
       id: this.data?.id || uuid(),
       RestaurantName: this.name,
       Drink: this.drink,
       Review: this.review
     }
-
+    //if statement to point the app to the correct method for creating a new row or editing a current row.
     if (this.data?.id) {
       return this.service.onRowEdit(info)
     }
-
     this.service.create(info)
   }
-
+//used event binding to handle the input by the user
   setName(event: Event) {
     this.name = (event.target as HTMLInputElement).value
   }
-
   setDrink(event: Event) {
     this.drink = (event.target as HTMLInputElement).value
   }
